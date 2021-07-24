9d2167018049012f48e6e2808fedcb16effc7577
Mahmudul Hasan <ctw00696@criticaltechworks.com>
2021-07-24 14:50:52 +0100

    python project added

---
+++ app.py
@@ -0,0 +1,42 @@
+from types import MethodType
+from flask import Flask
+from flask import json
+import logging
+import datetime
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    app.logger.info('Main request successfull')
+    return "Hello World!"
+
+
+@app.route("/status")
+def healthCheck():
+    response = app.response_class(
+        response=json.dumps({"result": "OK -healthy"}),
+        status=200,
+        mimetype="application/json"
+    )
+    app.logger.info('Status request successfull')
+    return response
+
+
+@app.route("/metrics")
+def metricsCheck():
+    response = app.response_class(
+        response=json.dumps({"status": "success", "code": 0, "data": {
+                            "UserCount": 140, "UserCountActive": 23}}),
+        status=201,
+        mimetype='application/json'
+    )
+
+    app.logger.info('Metrics request successfull')
+    return response
+
+
+if __name__ == "__main__":
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+    app.run(host='0.0.0.0')
